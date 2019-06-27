273fbfcf1f9ed2422828a0119c65f8062d3beba2
Lalo Lucatero <lalodev@outlook.com>
2019-06-27 18:36:16 +0000

    updates...

--- server/index.js
+++ server/index.js
@@ -5,7 +5,9 @@ const socketIO = require('socket.io');
 // our localhost port
 const port = 8080;
 
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 // our server instance
 const server = http.createServer(app);
@@ -17,26 +19,16 @@ let players = {};
 const io = socketIO(server);
 
 io.on('connection', socket => {
-    io.sockets.emit('getPlayers', players);
-
-    io.on('getId', (currentPlayer) => {
-        if (players[currentPlayer.id] === undefined){
-            currentPlayer.id = Object.keys(players).length;
-        }
-        players[currentPlayer.id] = currentPlayer;
-        console.log(players)
-        io.sockets.emit('getId', currentPlayer);
-    });  
-  
-    io.on('players', (currentPlayer) => {
-        players[currentPlayer.id] = currentPlayer;
-        console.log(players)
+    io.sockets.emit('players', players);
+    
+    socket.on('players', (currentPlayer) => {
+        players[currentPlayer.i] = currentPlayer;
         io.sockets.emit('players', players);
     });
   
     // disconnect is fired when a client leaves the server
     socket.on('disconnect', () => {
-        console.log('user disconnected');
+        console.log('user disconnected', socket.id);
     });
 });
 
--- server/socketServices/Players.js
+++ server/socketServices/Players.js
@@ -1,10 +1,47 @@
-export function reloadPlayers(currentPlayer, playerArr){
+function reloadPlayers(currentPlayer, playerArr){
     playerArr[currentPlayer.id] = currentPlayer;
 }
 
-export function getIds(currentPlayer, playerArr){
+function getIds(currentPlayer, playerArr){
     if (playerArr[currentPlayer.id] === undefined){
         currentPlayer.id = Object.keys(playerArr).length;
     }
     playerArr[currentPlayer.id] = currentPlayer;
 }
+
+
+let players = { '0':
+   { x: 1,
+     y: 120,
+     color: 'red',
+     h: 32,
+     w: 32,
+     image: {},
+     canMove: true,
+     id: 0 },
+  '1':
+   { x: 1,
+     y: 120,
+     color: 'red',
+     h: 32,
+     w: 32,
+     image: {},
+     canMove: true,
+     id: 1 } }
+
+const p1 = { 
+    x: 100,
+    y: 100,
+    id: 0
+} 
+     
+const p2 = { 
+    x: 999,
+    y: 999,
+    id: 1
+} 
+
+for (const prop in players) {
+        if(players[prop].id !== p1.id)
+        console.log(players[prop]);
+    }
